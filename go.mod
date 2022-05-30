e204f8d95aeb3569d4e39af583e245d8f9ab0a71
Wolfgang Huse <wolfgang.huse@nutanix.com>
2022-05-30 19:06:59 +0200

    linux build

--- go.mod
+++ go.mod
@@ -1,13 +1,3 @@
-module github.com/salrashid123/gce_metadata_server
+module metadata_service
 
-go 1.15
-
-require (
-	cloud.google.com/go v0.79.0
-	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/gorilla/mux v1.7.3
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
-	golang.org/x/oauth2 v0.0.0-20210313182246-cd4f82c27b84
-	google.golang.org/api v0.44.0-impersonate-preview
-	google.golang.org/genproto v0.0.0-20210319143718-93e7006c17a6
-)
+go 1.17
--- main.go
+++ main.go
@@ -51,10 +51,7 @@ type metadata struct {
 }
 func main() {
 
-    mydata := &metadata{
-		Hostname: "Pigeon",
-        Region: "fra",
-	}
+    mydata := &metadata{}
 
 	
     myJsonString := `{"hostname":"centos-s-1vcpu-1gb-fra1-01","region":"fra1","interfaces":{"private":[{"ipv4":{"ip_address":"10.114.0.2","netmask":"255.255.240.0","gateway":"10.114.0.1"},"mac":"ae:8a:a5:54:06:27","type":"private"}],"public":[{"ipv4":{"ip_address":"142.93.108.93","netmask":"255.255.240.0","gateway":"142.93.96.1"},"anchor_ipv4":{"ip_address":"10.19.0.5","netmask":"255.255.0.0","gateway":"10.19.0.1"},"mac":"ca:cd:a7:1a:4d:7e","type":"public"}]},"floating_ip":{"ipv4":{"active":false}},"dns":{"nameservers":["67.207.67.2","67.207.67.3"]}}`
@@ -66,10 +63,7 @@ func main() {
 	fmt.Println(string(data))
 
     http.HandleFunc("/metadata/v1.json", func(w http.ResponseWriter, r *http.Request) {
-        //fmt.Fprintf(w, "[{'droplet_id': 2756294,'hostname': 'sample-droplet','vendor_data': '#cloud-config','public_keys': [],'region': 'nyc3','interfaces': {'private': [],'public': []},'floating_ip': {},'dns': {},'features': {}}]" )
         fmt.Fprint(w,string(data))
-        //fmt.Fprint(w,myJsonString)
-        
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil))
